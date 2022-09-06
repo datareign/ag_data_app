a7b2246fa0238219e7963f660a71008e867c9482
datareign <mike.griffel@gmail.com>
2022-09-05 22:03:44 -0600

    added crop assignment viewer and other fixes

--- ag_data_app.py
+++ ag_data_app.py
@@ -23,17 +23,25 @@ db=firestore.Client(credentials=creds,project='agdata-f5a79')
 years=[2022,2023]
 
 st.title('ProGro Data Management System')
-menu=['Crop Assignment','Inputs Logging']
+menu=['View Crop Assignments','Assign Crops','Inputs Logging']
 choice=st.sidebar.selectbox('Menu',menu)
 
-if choice=='Crop Assignment':
-    st.session_state.counter=0
+if choice=='View Crop Assignments':
+    year=st.selectbox('Crop Year',years)
+    df_list=[]
+    crop_refs=db.collection('crop_assignments')
+    for doc in crop_refs.stream():
+        df_list.append(pd.DataFrame(doc.to_dict(),index=[0]))
+    df=pd.concat(df_list)
+    df=df[df['crop_year']==int(year)]
+    st.write(df[['client','farm','field','crop_year','acres','crop','variety']])
+
+elif choice=='Assign Crops':
     now=datetime.datetime.utcnow()
     clients=np.sort(fields['Client'].unique())
     crops=np.sort(crops_variety['crop'].unique())
     
     st.subheader('Crop and Variety Assignment')
-    #with st.form(key='form0'):
     crop=st.selectbox('Crop',crops,0)
     varieties=np.sort(crops_variety[crops_variety['crop']==crop]['variety'].unique())
     variety=st.selectbox('Variety',varieties)
@@ -42,14 +50,11 @@ if choice=='Crop Assignment':
     client=st.selectbox('Client',clients)
     farms=np.sort(fields[fields['Client']==client]['Farm'].unique())
     farm=st.selectbox('Farm',farms)
-    #submit_button1=st.form_submit_button()
 
-#with st.form(key='form2'):
     fields_sub=fields[fields['Client']==client]
     fields_sub=fields_sub[fields_sub['Farm']==farm]
     fields_list=np.sort(fields_sub['Field'].unique())
     fields_list=st.multiselect('Fields',fields_list)
-    #submit_button2=st.form_submit_button()
     
     if st.button('SUBMIT'):
         for field in fields_list:
@@ -68,30 +73,6 @@ if choice=='Crop Assignment':
                          'crop':crop,
                          'variety':variety})
             
-        #    
-        #    
-        #    crop_assignments.loc[ca_i,'log_datetime']=now
-        #    crop_assignments.loc[ca_i,'unq_fldid']=field_row.iloc[0]['unq_fldid']
-        #    crop_assignments.loc[ca_i,'year']=year
-        #    crop_assignments.loc[ca_i,'client']=client
-        #    crop_assignments.loc[ca_i,'farm']=farm
-        #    crop_assignments.loc[ca_i,'field']=field
-        #    crop_assignments.loc[ca_i,'crop']=crop
-        #    crop_assignments.loc[ca_i,'variety']=variety
-        #    ca_i+=1
-        
-        #crop_assignments['year']=crop_assignments['year'].astype(int)
-        #crop_assignments['unq_fldid']=crop_assignments['unq_fldid'].astype(int)
-        #crop_assignments.to_csv('data/crop_assignments.csv',index=False)
-        #st.write('The crop was assigned to the fields')
-        #st.write(st.session_state.keys())
-        #st.write(crop_assignments[['log_datetime','client','farm'
-        #                           ,'field','crop','variety']])
-        #st.session_state.counter=0
-        #st.experimental_rerun() 
-        
-        
-
     
 elif choice=='Inputs Logging':
     st.subheader('Inputs Logging')
